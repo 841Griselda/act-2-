028ef71b93d7de61c52db3d9dd2cafeaada7474a
brenda griselda morales velazquez <brendamora841@gmail.com>
2025-12-02 06:38:04 +0100

    Add files via upload

---
+++ Datos personales.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACTIVIDAD_2
+{
+    public partial class DATOS_PERSONALES : Form
+    {
+        public DATOS_PERSONALES()
+        {
+            InitializeComponent();
+        }
+
+        private void DATOS_PERSONALES_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { }
+            private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            MENU menu = new MENU();
+            menu.Show();
+            this.Hide();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { }
+            private void btnRegresar2_Click(object sender, EventArgs e)
+        {
+            MENU menu = new MENU();
+            menu.Show();
+            this.Hide();
+        }
+
+    }
+}
+
+
